f3d61d044d66e5bf2e18751bc0bec74cba34d73c
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-06-21 20:57:00 +0530

    learn to delete file from git command

--- array/13.py
+++
@@ -1,8 +0,0 @@
-# intersection of two arrays
-
-arr1 = list(map(int , input("Enter first set : ").split()))
-arr2 = list(map(int , input("Enter second set : ").split()))
-
-intersection = set(arr1).intersection(arr2)
-
-print("Intersection elements : ", *intersection)
